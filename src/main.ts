7ac9468c6ae20a0f9076471630f7a24e0dbb177f
Riley H <rileyhan.7@gmail.com>
2023-05-03 17:15:11 +0800

    feat: install bootstrap and added navbar routing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/bootstrap.scss'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App)
 
--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/About">About</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/Contact-us">Contact Us</RouterLink>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
 <h1>Homepage</h1>
+<RouterView />
     </div>
 </template>
 
