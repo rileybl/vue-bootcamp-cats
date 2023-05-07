30e390a6a427ede63df4b2e6b380dcefd95e11d0
rileybl <riley.han@thebitlab.co>
2023-05-07 15:47:18 +0800

    feat: added object fit into scss files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './styles/bootstrap.scss'
+import './styles/core.scss'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
